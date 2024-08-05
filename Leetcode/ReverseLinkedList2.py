ee81be23d507b237a6052e00e0c8c730bd9b31f5
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-08-05 22:43:33 +0600

    Add files via upload

---
+++ Leetcode/ReverseLinkedList2.py
@@ -0,0 +1,42 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
+        if not head or left == right:
+            return head
+
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+
+        for _ in range(left - 1):
+            prev = prev.next
+        start = prev.next
+        then = start.next
+
+        for _ in range(right - left):
+            start.next = then.next
+            then.next = prev.next
+            prev.next = then
+            then = start.next
+
+        return dummy.next
+
+def create_linked_list(lst):
+    dummy = ListNode()
+    current = dummy
+    for value in lst:
+        current.next = ListNode(value)
+        current = current.next
+    return dummy.next
+
+def linked_list_to_list(head):
+    lst = []
+    current = head
+    while current:
+        lst.append(current.val)
+        current = current.next
+    return lst
