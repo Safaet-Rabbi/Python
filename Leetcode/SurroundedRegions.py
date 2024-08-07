6b3f864f021e7657c2e069ab80e849dfab3239c9
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-08-07 22:21:13 +0600

    Add files via upload

---
+++ Leetcode/SurroundedRegions.py
@@ -0,0 +1,32 @@
+class Solution:
+    def solve(self, board):
+        if not board or not board[0]:
+            return
+        m, n = len(board), len(board[0])
+        def dfs(x, y):
+            if x < 0 or x >= m or y < 0 or y >= n or board[x][y] != 'O':
+                return
+            board[x][y] = 'E'
+            dfs(x + 1, y)
+            dfs(x - 1, y)
+            dfs(x, y + 1)
+            dfs(x, y - 1)
+        
+        for i in range(m):
+            if board[i][0] == 'O':
+                dfs(i, 0)
+            if board[i][n - 1] == 'O':
+                dfs(i, n - 1)
+        
+        for j in range(n):
+            if board[0][j] == 'O':
+                dfs(0, j)
+            if board[m - 1][j] == 'O':
+                dfs(m - 1, j)
+        
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                elif board[i][j] == 'E':
+                    board[i][j] = 'O'
