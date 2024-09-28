df0ca7e1f2eba0f9fe92a2fef39af0aeeb8f951b
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-09-28 15:46:10 +0600

    Add files via upload

---
+++ Codeforce/1133C.py
@@ -0,0 +1,12 @@
+def balanced_team(n, skills):
+    skills.sort()
+    left = 0
+    max_team_size = 0
+    for right in range(n):
+        while skills[right] - skills[left] > 5:
+            left += 1     
+        max_team_size = max(max_team_size, right - left + 1) 
+    return max_team_size
+n = int(input())
+skills = list(map(int, input().split()))
+print(balanced_team(n, skills))
