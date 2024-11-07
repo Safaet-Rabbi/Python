6c43e5d590e4b27ade9b834492d1321522551d22
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-11-07 23:42:07 +0600

    Add files via upload

---
+++ Leetcode/PartitionList.py
@@ -0,0 +1,21 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        before_head = ListNode(0)
+        after_head = ListNode(0)      
+        before = before_head
+        after = after_head     
+        while head:
+            if head.val < x:
+                before.next = head
+                before = before.next
+            else:
+                after.next = head
+                after = after.next
+            head = head.next    
+        before.next = after_head.next
+        after.next = None       
+        return before_head.next
