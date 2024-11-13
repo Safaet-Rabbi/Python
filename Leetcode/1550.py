b8419928283f2d4687ca47425bfb21b437b28f77
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-11-13 23:01:55 +0600

    Delete Leetcode/1550.py

--- Leetcode/1550.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def threeConsecutiveOdds(self, arr):
-        for i in range(len(arr) - 2):
-            if arr[i] % 2 != 0 and arr[i + 1] % 2 != 0 and arr[i + 2] % 2 != 0:
-                return True
-        return False
-sol = Solution()
-print(sol.threeConsecutiveOdds([2, 6, 4, 1])) 
-print(sol.threeConsecutiveOdds([1, 2, 34, 3, 4, 5, 7, 23, 12]))
