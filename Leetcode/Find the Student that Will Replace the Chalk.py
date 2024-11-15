0be5d5806658757af40c6bf0e56b965750cd6888
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-11-15 22:06:06 +0600

    Add files via upload

---
+++ Leetcode/Find the Student that Will Replace the Chalk.py
@@ -0,0 +1,8 @@
+class Solution:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        total_chalk = sum(chalk)
+        k %= total_chalk    
+        for i, chalk_needed in enumerate(chalk):
+            if k < chalk_needed:
+                return i
+            k -= chalk_needed
