54ed30d294813a9a009ea09c680eaed7f209cf64
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2024-11-30 21:46:15 +0600

    Add files via upload

---
+++ Delete Nodes From Linked List Present in Array.py
@@ -0,0 +1,16 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def modifiedList(self, nums: list[int], head: Optional[ListNode]) -> Optional[ListNode]:
+        num_set = set(nums)      
+        dummy = ListNode(0)
+        dummy.next = head
+        current = dummy       
+        while current.next:
+            if current.next.val in num_set:
+                current.next = current.next.next
+            else:
+                current = current.next       
+        return dummy.next
