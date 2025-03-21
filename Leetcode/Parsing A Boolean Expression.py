f65180dc0802b4d20db884b79b7904de756c78a7
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2025-03-21 22:11:13 +0600

    Add files via upload

---
+++ Leetcode/Parsing A Boolean Expression.py
@@ -0,0 +1,22 @@
+class Solution:
+    def parseBoolExpr(self, expression: str) -> bool:
+        stack = []
+        for char in expression:
+            if char == ',':
+                continue 
+            elif char == ')':
+                sub_expr = []
+                while stack[-1] != '(':
+                    sub_expr.append(stack.pop())
+                stack.pop()  
+                
+                operator = stack.pop()  
+                if operator == '!':
+                    stack.append('t' if sub_expr[0] == 'f' else 'f')
+                elif operator == '&':
+                    stack.append('t' if all(x == 't' for x in sub_expr) else 'f')
+                elif operator == '|':
+                    stack.append('t' if any(x == 't' for x in sub_expr) else 'f')
+            else:
+                stack.append(char)
+        return stack[0] == 't'
