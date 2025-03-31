25e235896775986d01b669f44f25b47161f41bf4
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2025-03-31 21:03:14 +0600

    Add files via upload

---
+++ Leetcode/Minimum Add to Make Parentheses Valid.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        left = right = 0
+        for char in s:
+            if char == '(':
+                left += 1
+            elif left > 0:
+                left -= 1
+            else:
+                right += 1
+        return left + right
