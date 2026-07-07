3b7ad6916e710c6b89dc3db631b4c910d1843c66
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2026-07-07 23:16:53 +0600

    Add files via upload

---
+++ Leetcode/Concatenate Non-Zero Digits and Multiply by Sum I.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sumAndMultiply(self, n: int) -> int:
+        x = 0
+        s = 0
+
+        for ch in str(n):
+            if ch != '0':
+                d = int(ch)
+                x = x * 10 + d
+                s += d
+
+        return x * s
