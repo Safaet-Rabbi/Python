b5284783c03735825c8b0f6eecd16a37ab03c5ab
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2026-09-03 19:42:16 +0600

    Add files via upload

---
+++ Leetcode/Construct Uniform Parity Array II.py
@@ -0,0 +1,6 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        mn = min(nums1)
+        if mn & 1:
+            return True
+        return all(x % 2 == 0 for x in nums1)
