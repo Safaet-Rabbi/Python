6fff32eaa5ee270818c7539fb9f20a88405cdde4
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2026-09-08 10:26:32 +0600

    Add files via upload

---
+++ Leetcode/Count Commas in Range.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countCommas(self, n: int) -> int:
+        ans = 0
+
+        if n >= 1000:
+            ans += n - 999
+
+        if n >= 1000000:
+            ans += n - 999999
+
+        return ans
