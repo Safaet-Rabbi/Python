b76ca6873e411102312c2c97cbc1744a29393fbc
Safaet-Rabbi <86919997+Safaet-Rabbi@users.noreply.github.com>
2026-09-26 19:36:10 +0600

    Add files via upload

---
+++ Leetcode/Evaluate the Bracket Pairs of a String.py
@@ -0,0 +1,16 @@
+class Solution:
+    def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
+        mp = dict(knowledge)
+        res = []
+        i = 0
+
+        while i < len(s):
+            if s[i] == '(':
+                j = s.index(')', i)
+                res.append(mp.get(s[i + 1:j], '?'))
+                i = j + 1
+            else:
+                res.append(s[i])
+                i += 1
+
+        return ''.join(res)
